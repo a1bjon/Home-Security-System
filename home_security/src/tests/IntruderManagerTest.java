a5d0b392d3ce5d899718bec0218932395871d3f9
a1bjon <81479352+a1bjon@users.noreply.github.com>
2023-04-30 20:24:00 +0100

    Add files via upload

--- home_security/src/tests/IntruderManagerTest.java
+++ home_security/src/tests/IntruderManagerTest.java
@@ -6,7 +6,6 @@ import static org.junit.Assert.*;
 public class IntruderManagerTest {
     @Test
     public void testLoadIntruders() {
-        int random = (int)(Math.random() * 500000 + 1);
         intruderManager im = new intruderManager();
         im.loadIntruders();
         assertEquals(true, im.intruders.size() > 0);
