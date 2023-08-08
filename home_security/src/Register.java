80d510eec02319f6aa19bc3dba5a6dba1a880888
a1bjon <81479352+a1bjon@users.noreply.github.com>
2023-08-08 10:19:30 +0100

    Update Register.java

--- home_security/src/Register.java
+++ home_security/src/Register.java
@@ -4,7 +4,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
-
 import accountUtilities.accountManager;
 
 public class Register extends JFrame{
